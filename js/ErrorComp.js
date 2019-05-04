8bf2e71d07d72a8cb4c1c05cfd2f43103d0c6760
Boris Titarenko <zik2009@yandex.ru>
2019-05-05 00:16:54 +0400

    1

--- js/ErrorComp.js
+++ js/ErrorComp.js
@@ -1,9 +1,9 @@
 Vue.component('error', {
   template: `
-    <div class="error">
-      <strong>Ошибка!</strong>
-      <br>
-      <slot></slot>
-    </div>
-  `
+		    <div class="error">
+		      	<strong>Ошибка!</strong>
+		      	<br>
+		      	<slot></slot>
+		    </div>
+		  `
 })
